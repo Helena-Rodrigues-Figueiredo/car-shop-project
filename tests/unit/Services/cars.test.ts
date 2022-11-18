4776b1489e84844ea9741c4e349cd190207ee613
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-11-18 18:07:52 -0300

    feat: cria rota para cadastrar um carro

--- tests/unit/Services/cars.test.ts
+++ tests/unit/Services/cars.test.ts
@@ -2,8 +2,7 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import { Model } from 'mongoose';
 import ICar from '../../../src/Interfaces/ICar';
-import CarServise from '../../../src/Services/CarServise';
-import Car from '../../../src/Domain/Car';
+import CarService from '../../../src/Services/CarService';
 
 describe('Verifica se é possível cadastrar um carro', function () {
   it('Carro é cadastrado com SUCESSO', async function () {
@@ -16,21 +15,21 @@ describe('Verifica se é possível cadastrar um carro', function () {
       doorsQty: 4,
       seatsQty: 5,
     };
-    const carOutput: Car = new Car(
-      '6348513f34c397abcad040b2',
-      'Marea',
-      2002,
-      'Black',
-      true,
-      15.990,
-      4,
-      5,
-    );
+    const carOutput: ICar = {
+      id: '6348513f34c397abcad040b2',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
 
     sinon.stub(Model, 'create').resolves(carOutput);
 
-    const service = new CarServise();
-    const result = await service.createCar(carInput);
+    const service = new CarService();
+    const result = await service.create(carInput);
 
     expect(result).to.be.deep.equal(carOutput);
   });
